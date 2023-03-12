65799863fd6eba20e633dfb61591461bf9c879b1
마름모 <maehwa03@outlook.kr>
2023-03-12 10:32:05 +0100

    신규 파일 추가

---
+++ Python/gradeCalc.py
@@ -0,0 +1,28 @@
+print('Grades Calculator')
+
+def grade(intScore):
+    if intScore >= 85:
+        grade = 'A*'
+    elif intScore >= 75:
+        grade = 'A'
+    elif intScore >= 65:
+        grade = 'B'
+    elif intScore >= 55:
+        grade = 'C'
+    elif intScore >= 45:
+        grade = 'D'
+    elif intScore >= 35:
+        grade = 'E'
+    else:
+        grade = 'U'
+    return grade
+
+scoreCount = 1
+score = ''
+
+while score != 'S':
+    score = input('Enter Score ' + str(scoreCount) + ' (/100) or S to stop: ')
+
+    if score != 'S':
+        print('Grade', str(scoreCount), '=', grade(int(score)))
+        scoreCount += 1
